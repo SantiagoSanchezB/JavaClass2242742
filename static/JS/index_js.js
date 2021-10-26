3ad1046f13f4ed393c331b5d001e7cb946970539
SantiagoS <dssanchez752@misena.edu.co>
2021-10-26 17:36:40 -0500

    Class Level 2

--- static/JS/index_js.js
+++ static/JS/index_js.js
@@ -24,8 +24,41 @@ function mostrarSugerencia (selectPais){
 
     xmlhttp = new XMLHttpRequest();
     xmlhttp.onreadystatechange = function (){
-        
+            if (xmlhttp.readyState == 4 && xmlhttp.status == 200){
+                var jsonResponse = xmlhttp.responseText;
+                var objetoJSON = JSON.parse(jsonResponse);
+                paisesRecibidos = objetoJSON.listadoPaises.pais;
+
+                for (var i = 0; i < paisesRecibidos.length; i++){
+                    var nombrePais = objetoJSON.listadoPaises.pais[i].nombre;
+                    if (nombrePais == paisElegido){
+                        document.getElementById("txtInformacion").innerHTML = "El pais Recibidos es " + nombrePais + " y tiene indice " + i;
+                        var ciudadesPais = objetoJSON.listadoPaises.pais[i].ciudadImportante;
+                        mostrarCiudades (ciudadesPais);
+                    }
+                }
+            }
     }
 
+    xmlhttp.open("GET", "listadoPaises.json?nocache=' + (new Date()).getTime()'")
+    xmlhttp.send();
 
+
+}
+
+function mostrarCiudades(arrayCiudades){
+    var nodoMostrarResultados = document.getElementById("listaCiudades");
+    if (!arrayCiudades){
+        nodoMostrarResultados.innerHTML = "";
+        return;
+    }
+
+    var contenidoAMostrar = "";
+    for (var i = 0; i < arrayCiudades.length; i++){
+        contenidoAMostrar = contenidoAMostrar + '<div id="ciudades' + i + '">';
+        contenidoAMostrar += '<a href="https://www.google.com">' + arrayCiudades[i] + '</a></div>';
+    }
+    if (contenidoAMostrar) {
+        nodoMostrarResultados.innerHTML = contenidoAMostrar;
+    }
 }
